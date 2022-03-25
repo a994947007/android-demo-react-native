62a75e6122543ae12409049e61ad788e3f1b7d83
a994947007 <994947007@qq.com>
2022-03-26 02:48:22 +0800

    react native动态化方案

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+import Nav from './src/navigation/nav';
+
+export default function App() {
+  return (
+    <View>
+      <Text>
+        {"fsgsfdgssssss"}
+      </Text>
+    </View>
+  );
+}
+/*
+<View style={{flex: 1}}>
+<Nav></Nav>
+</View>
+*/
